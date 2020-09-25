ad8b51ee6405f65b353c8b69ef7098dd3f0383f6
Russel Rai <leon.amorphy@googlemail.com>
2020-09-25 20:20:30 +0100

    created recursion practice codes

---
+++ RecursionExample.java
@@ -0,0 +1,7 @@
+public class RecursionExample {
+
+  /**
+   * Recursion is the technique of making a function call itself.
+   * It provides a way to break complicated problems into simpole problems that are easier to solve.
+   */
+}
