631b67022216e954b8d84b9febed53ac99363f91
Russel Rai <leon.amorphy@googlemail.com>
2020-09-27 18:00:45 +0100

    added recursion example

--- RecursionExample.java
+++ RecursionExample.java
@@ -1,7 +1,20 @@
 public class RecursionExample {
 
   /**
-   * Recursion is the technique of making a function call itself.
-   * It provides a way to break complicated problems into simpole problems that are easier to solve.
+   * Recursion is the technique of making a function call itself. It provides a
+   * way to break complicated problems into simpole problems that are easier to
+   * solve.
    */
+  public static void main(String[] args) {
+    int result = sum(10);
+    System.out.println("Result: " + result);
+  }
+
+  public static int sum(int k) {
+    if (k > 0) {
+      return k + sum(k - 1);
+    } else {
+      return 0;
+    }
+  }
 }
