eee86883c0af1392090555c0ad1a25948554f57b
Russel Rai <leon.amorphy@googlemail.com>
2020-10-01 11:16:45 +0100

    deleted extra objects

--- ClassObjectExample.java
+++ ClassObjectExample.java
@@ -1,16 +1,17 @@
 public class ClassObjectExample {
   public static void main(String[] args) {
     OtherClass myObj = new OtherClass();
+    OtherClass my2Obj = new OtherClass();
 
+    my2Obj.x = 30;
     System.out.println(myObj.x);
+    System.out.println(my2Obj.x);
     runObject();
   }
 
   static void runObject() {
     OtherClass myObject = new OtherClass();
-    OtherClass mySecondObj = new OtherClass();
 
     System.out.println(myObject.x);
-    System.out.println(mySecondObj.x);
   }
 }
--- OtherClass.java
+++ OtherClass.java
@@ -2,5 +2,5 @@ public class OtherClass {
   // x is an attribute of the class or a variables within a class
   // class attributes are also known as "fields"
   int x = 5;
-  int y = 3;
+  int y;
 }
